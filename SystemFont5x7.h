a13b403979b924b65937218dd11715d5a85bc0ea
Angus Gratton <gus@projectgus.com>
2014-02-18 16:18:23 +1100

    Add 'const' to PROGMEM font declarations

--- SystemFont5x7.h
+++ SystemFont5x7.h
@@ -45,7 +45,7 @@
 
 #define SystemFont5x7 System5x7
 
-static uint8_t System5x7[] PROGMEM = {
+const static uint8_t System5x7[] PROGMEM = {
     0x0, 0x0, // size of zero indicates fixed width font, actual length is width * height
     0x05, // width
     0x07, // height
